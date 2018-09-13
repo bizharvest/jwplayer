3144e4bb84194c1c87d740a3d0ed95269eefd00d
karimJWP <karimJWP@github.com>
2018-09-13 19:06:56 -0400

    pass response type

--- src/js/utils/language.js
+++ src/js/utils/language.js
@@ -45,5 +45,5 @@ export function isTranslationAvailable(language) {
 
 export function loadJsonTranslation(base, languageCode, oncomplete, onerror) {
     const url = base + 'translations/' + languageCode + '.json';
-    ajax({ url, oncomplete, onerror, otherOptions: { responseType: 'json' } });
+    ajax({ url, oncomplete, onerror, responseType: 'json' });
 }
